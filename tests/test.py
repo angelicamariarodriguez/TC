23ce71394fda6214ba3aa1a3b53ecac65ff956cc
Angelica <am.rodriguez12@uniandes.edu.co>
2022-11-01 22:01:48 -0500

    coverage5

---
+++ tests/test.py
@@ -0,0 +1,13 @@
+import unittest
+import sys
+
+
+
+#from flaskr.modelos import db, Cancion, Usuario, CancionSchema
+
+
+class ComentariosTest(unittest.TestCase):
+    
+    @staticmethod
+    def create_app(self):
+        c = 1
